c164e5b35ec67860078875abeac34b65af21c147
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-08-20 18:09:30 -0700

    Primer commit

---
+++ src/paquete/ifelsejava.java
@@ -0,0 +1,16 @@
+package paquete;
+
+public class ifelsejava {
+
+    public static void main(String[] args) {
+        int var = 2;
+
+        if (var == 5) {
+            System.out.println("var si es 5");
+        } else {
+            System.out.println("no es es 5");
+        }
+    }
+
+
+}
