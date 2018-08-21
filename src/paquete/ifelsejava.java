94b1a6cab75737038185b866906e5fcaa8154282
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-08-21 10:28:53 -0700

    Se realizo practica comparaciones

--- src/paquete/ifelsejava.java
+++ src/paquete/ifelsejava.java
@@ -2,14 +2,58 @@ package paquete;
 
 public class ifelsejava {
 
-    public static void main(String[] args) {
-        int var = 2;
 
-        if (var == 5) {
-            System.out.println("var si es 5");
+    public static void main(String[] args) {
+        /**
+         * Operadores logicos de comparacion
+         * >   Mayor que
+         * <   Menor que
+         * >=  Mayor o igual
+         * <=  Menor o igual
+         * ==  igual
+         * ||  O
+         * &&  and
+         * !=  Diferente
+         *
+         */
+        int var = 6;
+        int var2 = 10;
+        boolean cond;
+        cond = true;
+
+        System.out.println("Comparacion igualdad");
+        if (var == 6) {
+            cond = false;
+            System.out.println("var si es 6");
+        } else {
+            cond = true;
+            System.out.println("no es es 6");
+        }
+        System.out.println("------------------------------");
+
+        System.out.println("Comparacion bolleana");
+        if (cond) {
+            System.out.println("La variable cond es verdadera");
+        } else {
+            System.out.println("La variable cond es falso");
+        }
+        System.out.println("------------------------------");
+
+        System.out.println("Comparacion con dos variables que tienen que ser verdaderas");
+        if ((var == 6) && (var2 > 5)) {
+            System.out.println("Las dos variables dan verdadero");
+        } else {
+            System.out.println("Las dos variables dan falso");
+        }
+        System.out.println("------------------------------");
+
+        System.out.println("Negando una comparacion");
+        if (var != 6) {
+            System.out.println("var es diferente de 6");
         } else {
             System.out.println("no es es 5");
         }
+
     }
 
 
