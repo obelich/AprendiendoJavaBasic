fdbb06a07a2c958f0d057a5414fd3da42d1db1c0
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-08-29 17:04:34 -0700

    Ahora hace un try catch para comprobar errores de conexion

--- Spring/src/com/novellius/MainApp.java
+++ Spring/src/com/novellius/MainApp.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 import com.novellius.dao.AdministratorsDao;
 import com.novellius.pojo.Administrators;
@@ -12,22 +14,29 @@ import com.novellius.pojo.Administrators;
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring_config.xml");
-		
+
 		AdministratorsDao administratorsDao = (AdministratorsDao) applicationContext.getBean("administratorsDao");
 		Timestamp ts = new Timestamp(new Date().getTime());
 		Administrators administrator = new Administrators();
 		administrator.setRole("Gerente");
 		administrator.setNames("Miguel Amezcua");
 		administrator.setCreated_at(ts);
-//		AdministratorsDao administratorsDao = applicationContext.getBean("administratorsDao");
-		
-		if (administratorsDao.save(administrator) ) {
-			System.out.println("Si se guardo");
-		} else {
-			System.out.println("No se guardo");
+
+		try {
+			if (administratorsDao.save(administrator)) {
+				System.out.println("Si se guardo");
+			}
+
+		} catch (CannotGetJdbcConnectionException ex) {
+			// TODO: handle exception
+			ex.printStackTrace();
+		} catch (DataAccessException ex) {
+			// TODO: handle exception
+			ex.printStackTrace();
+
 		}
-		
-		((ClassPathXmlApplicationContext  )applicationContext).close();
+
+		((ClassPathXmlApplicationContext) applicationContext).close();
 	}
 
 }
