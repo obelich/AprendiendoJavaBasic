278b17c11a1e919ef5b92a038fda60f70178bbbd
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-09-16 19:47:47 -0700

    ternimado ejemplo de cilco for y siguiente parte creacion de clases

---
+++ FundamentosJava/Leccion04/CreacionClasesObjetos/src/Persona/Persona.java
@@ -0,0 +1,15 @@
+package Persona;
+
+public class Persona {
+
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+
+    public void desplegarNombre() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido Paterno: " + apellidoPaterno);
+        System.out.println("Apellido Materno: " + apellidoMaterno);
+
+    }
+}
