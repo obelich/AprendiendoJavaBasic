0915873a893eb809a87f297d81fead8e2dcd2650
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-09-21 15:25:24 -0700

    Juego sencillo de adivina el numero

--- fundamentos-java/AdivinaElNumero/src/AdivinaElNumero.java
+++ fundamentos-java/AdivinaElNumero/src/AdivinaElNumero.java
@@ -14,12 +14,29 @@ public class AdivinaElNumero {
 
 
         while(juegoActivo) {
+            int intentos = 0;
             int min = 0 ;
             int max = 100;
 
             int numeroJuego = (int) (Math.random() * ((max - min) +1));
 
-            System.out.println(numeroJuego);
+            System.out.printf("%s, he escogido un numero entre %d y %d, adivinalo ", nombreJugador, min, max);
+            int numeroJugador;
+
+            do {
+
+                System.out.println("Escoge un numero");
+                numeroJugador = entrada.nextInt();
+
+                if (numeroJuego < numeroJugador) {
+                    System.out.println("Muy alto, adivina otra vez \n");
+                } else if (numeroJuego > numeroJugador) {
+                    System.out.println("Muy bajo, adivina otra vez \n");
+                }
+                intentos++;
+            } while(numeroJuego != numeroJugador);
+
+            System.out.printf("Felicidades adivinaste con %d intentos \n", intentos);
 
             juegoActivo = false;
 
