a262a6226ecd5a1e3eb5b023a0ec87120ddcbbee
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-09-24 13:10:37 -0700

    Estudiando sobre las herencias

---
+++ fundamentos-java/Objetos/src/modulo4/herencia/Ejemplo.java
@@ -0,0 +1,10 @@
+package modulo4.herencia;
+
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        Futbolista futbolista = new Futbolista();
+        futbolista.correr();
+
+    }
+}
