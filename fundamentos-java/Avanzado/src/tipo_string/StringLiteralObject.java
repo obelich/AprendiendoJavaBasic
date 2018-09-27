84f02b1d57fe3952340be700054405c1bd0da7e8
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-09-27 12:14:06 -0700

    String literal y String objeto

---
+++ fundamentos-java/Avanzado/src/tipo_string/StringLiteralObject.java
@@ -0,0 +1,22 @@
+package tipo_string;
+
+public class StringLiteralObject {
+
+    public static void main(String[] args) {
+        //String literal
+        String s3 = "Devcode";
+        String s4 = "Devcode";
+
+        System.out.println(s3 == s4); //Regresara true
+
+        //String object
+        String s1 = new String("Devcode");
+        String s2 = new String("Devcode");
+
+        System.out.println(s1 == s2); //False
+
+        System.out.println(s3 == s2); //False
+
+    }
+
+}
