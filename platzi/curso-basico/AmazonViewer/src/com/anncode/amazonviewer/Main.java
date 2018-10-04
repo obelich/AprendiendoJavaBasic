6c3f900aa52937f99ed224779e6c2ba70f6875b4
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-10-04 12:45:49 -0700

    se continua con los metodos para el menu

--- platzi/curso-basico/AmazonViewer/src/com/anncode/amazonviewer/Main.java
+++ platzi/curso-basico/AmazonViewer/src/com/anncode/amazonviewer/Main.java
@@ -1,8 +1,17 @@
 package com.anncode.amazonviewer;
 
+
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        showMenu();
+
+    }
+
+    public static void showMenu() {
 
         int exit = 0;
         do {
@@ -12,10 +21,115 @@ public class Main {
             System.out.println("1. Movies");
             System.out.println("2. Series");
             System.out.println("3. Books");
+            System.out.println("4. Magazines");
+            System.out.println("5. Report");
+            System.out.println("6. Report Today");
             System.out.println("0. Exit");
+            int response = 1;
+            switch (response) {
+                case 0:
 
+                    break;
+                case 1:
+                    showMovies();
+                    break;
+                case 2:
+                    showSeries();
+                    break;
+                case 3:
+                    showBooks();
+                    break;
+                case 4:
+                    showMagazines();
+                    break;
+                case 5:
+                    makeReport();
+                    break;
+                case 6:
+                    makeReport(new Date());
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("....!! Selecciona una opcion!!....");
+                    System.out.println();
+                    break;
 
+            }
+
+        } while (exit != 0);
+
+    }
+
+    public static void showMovies() {
+        int exit = 0;
+
+        do {
+
+            System.out.println();
+            System.out.println(" :: MOVIES ::");
+            System.out.println();
 
         } while (exit != 0);
     }
+
+    public static void showSeries() {
+        int exit = 0;
+
+        do {
+
+            System.out.println();
+            System.out.println(" :: SERIES ::");
+            System.out.println();
+
+        } while (exit != 0);
+
+    }
+
+    public static void showBooks() {
+
+        int exit = 0;
+
+        do {
+
+            System.out.println();
+            System.out.println(" :: BOOKS ::");
+            System.out.println();
+
+        } while (exit != 0);
+    }
+
+    public static void showChapters() {
+        int exit = 0;
+
+        do {
+
+            System.out.println();
+            System.out.println(" :: CHAPTERS ::");
+            System.out.println();
+
+        } while (exit != 0);
+
+    }
+
+    public static void showMagazines() {
+
+        int exit = 0;
+
+        do {
+
+            System.out.println();
+            System.out.println(" :: MATAZINES ::");
+            System.out.println();
+
+        } while (exit != 0);
+    }
+
+    public static void makeReport() {
+
+    }
+
+    public static void makeReport(Date date) {
+
+    }
+
 }
