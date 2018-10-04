05b883e8e4372d934079358ca3f1e18a69967c61
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-10-04 14:47:31 -0700

    Generando getter and setters

--- platzi/curso-basico/AmazonViewer/src/com/anncode/amazonviewer/model/Serie.java
+++ platzi/curso-basico/AmazonViewer/src/com/anncode/amazonviewer/model/Serie.java
@@ -19,4 +19,76 @@ public class Serie {
         this.genre = genre;
         this.duration = duration;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean isViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    public int getSessionQuantity() {
+        return sessionQuantity;
+    }
+
+    public void setSessionQuantity(int sessionQuantity) {
+        this.sessionQuantity = sessionQuantity;
+    }
 }
